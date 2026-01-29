5b21e8eab0bab69cffce6467780db37de8551507
Snehal Sapkal <snehalsapkal1902@gmail.com>
2026-01-29 22:10:06 +0530

    String

--- Array/removing_duplicates.py
+++ Array/removing_duplicates.py
@@ -1,18 +1,5 @@
 def remove_duplicates(arr):
-    if len(arr) == 0:
-        return 0
+    return list(set(arr))
 
-    i = 0  # slow pointer
-
-    for j in range(1, len(arr)):
-        if arr[i] != arr[j]:
-            i += 1
-            arr[i] = arr[j]
-
-    return i + 1  # new length
-
-
-arr = [1,1,0,0,4,3,3]
-n = remove_duplicates(arr)
-
-print(arr[:n])  # [1, 2, 3, 4]
+arr = [3, 1, 2, 3, 4, 2]
+print(remove_duplicates(arr))
